98c363fa82871f011e16ee35234165182085831e
yunnysunny <yunnysunny@gmail.com>
2016-08-20 17:42:12 +0800

    Use console to print log if not given the instances of logger.

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+var _unshift = Array.prototype.unshift;
+var _push = Array.prototype.push;
+const MAP_LOG_LEVEL = {
+    DEBUG : {color : '\x1b[32m'},
+    TRACE : {color : '\x1b[34m'},
+    WARN : {color : '\x1b[33m'},
+    ERROR : {color : '\x1b[31m'}
+};
+
 var slogger = {
 
     /**
@@ -12,22 +21,35 @@ var slogger = {
         this.errorLogger = options.errorLogger;
         return this;
     },
+    print : function(args,level) {
+        level = (level || '').toUpperCase();
+        var config = MAP_LOG_LEVEL[level] || {color : ''};
+        _unshift.call(args, config.color + new Date().toString() + ' ['+level+']');
+        _push.call(args,'\x1b[0m');
+        console.info.apply(console, args);
+    },
     debug : function() {
         var debugLogger = this.debugLogger;
         if (debugLogger) {
             debugLogger.debug.apply(debugLogger,arguments);
+        } else {
+            this.print(arguments, 'DEBUG');
         }
     },
     trace : function() {
         var traceLogger = this.traceLogger;
         if (traceLogger) {
             traceLogger.trace.apply(traceLogger,arguments);
-        }        
+        } else {
+            this.print(arguments, 'TRACE');
+        }       
     },
     warn : function() {
         var errorLogger = this.errorLogger;
         if (errorLogger) {
             errorLogger.warn.apply(errorLogger,arguments);
+        } else {
+            this.print(arguments, 'WARN');
         }
         
     },
@@ -35,6 +57,8 @@ var slogger = {
         var errorLogger = this.errorLogger;
         if (errorLogger) {
             errorLogger.error.apply(errorLogger,arguments);
+        } else {
+            this.print(arguments, 'ERROR');
         }
         
     }
---
+++ test/test2.js
@@ -0,0 +1,6 @@
+var slogger = require('../index');
+
+slogger.debug('debug');
+slogger.trace('trace');
+slogger.warn('warn');
+slogger.error('error');

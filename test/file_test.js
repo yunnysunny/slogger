8c8075f104829fe72cab9e3e9c9fc0d91bb80627
yunnysunny <yunnysunny@gmail.com>
2018-07-05 18:31:13 +0800

    fix the error of delay writing file

--- test/file_test.js
+++ test/file_test.js
@@ -2,9 +2,11 @@ const path = require('path');
 var slogger = require('../index');
 
 slogger.init({
+    flushInterval:2000,
     logFiles:[
         {category:'warn',filename:path.join(__dirname , './log/warn.log')}
     ]
 });
-
-slogger.warn('first');
+for (var i=0;i<100;i++) {
+    slogger.warn(i);
+}

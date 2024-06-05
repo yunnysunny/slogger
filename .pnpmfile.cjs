e12fb714673c742079883e2416adcc19c23da8c8
yunnysunny <yunnysunny@gmail.com>
2024-06-05 15:16:16 +0800

    ci: fix syntax error on pnpm file

--- .pnpmfile.cjs
+++ .pnpmfile.cjs
@@ -1,23 +1,18 @@
 const fs = require('fs');
 const versionNode = Number(process.version.split('.')[0].substring(1));
-if (versionNode < 16) {
-    fs.unlinkSync('pnpm-lock.yaml');
-    console.log('delete pnpm-lock.yaml');
-} else {
-    console.log('pnpm-lock.yaml not delete');
-}
+
 function readPackage(pkg) {
     if (versionNode >= 16) {
         return pkg;
     }
     const { devDependencies } = pkg
-    if (devDependencies?.typescript) {
+    if (devDependencies.typescript) {
         devDependencies.typescript = '4.9.5'
     }
-    if (devDependencies?.typedoc) {
+    if (devDependencies.typedoc) {
         delete devDependencies.typescript
     }
-    if (devDependencies?.['typedoc-plugin-markdown']) {
+    if (devDependencies['typedoc-plugin-markdown']) {
         delete devDependencies['typedoc-plugin-markdown']
     }
     return pkg;
